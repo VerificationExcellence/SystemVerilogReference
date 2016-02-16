59df26d894627e521016954d3253ca51b78954c5
Ramdas M <mramdas@gmail.com>
2016-02-16 20:46:59 +0530

    New dual port ram file
    Simple Synchronous dual port ram module

---
+++ projects/DualPortRam/dualport_ram.sv
@@ -0,0 +1,30 @@
+//A synchronous dual port RAM that has two ports
+//One port for reading while one port for writing with seperate address
+//One data input port for writes
+//One data output port for reads
+//Memory contents are not initialized
+
+module dual_port_ram(
+              input  logic           clock       ,       // Clock
+              input  logic  [7:0]    data_in     ,       // input data
+              output logic  [7:0]    data_out    ,       // Output data
+              input  logic           write_en    ,       // 1 => write port enabled
+               input  logic  [7:0]   write_address ,     // Memory Write port address
+              input  logic           read_en,            // 1 => read port enabled
+              input  logic  [7:0]    read_address ,      // Memory Read port address
+              );
+ 
+
+//Memory array 
+logic [7:0] mem [255:0];
+ 
+//If enabled, write to the memory array
+always @ (posedge clock)
+begin
+  if(write_en)                      //Write port
+    mem[write_address] = data_in;
+  if(read_en)                       //Read port
+    data_out = mem[read_address];
+end
+ 
+endmodule
