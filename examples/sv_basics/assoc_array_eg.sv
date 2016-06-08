bd106846e17fea958a064bb235f7a5d05acb04ed
Ramdas M <mramdas@gmail.com>
2016-06-08 14:22:57 +0530

    demo assoc arrays

---
+++ examples/sv_basics/assoc_array_eg.sv
@@ -0,0 +1,26 @@
+//----------
+// Sample code to illustrate usage of associative arrays
+//  - Ramdas M  
+//----------
+module test;
+
+//Associative array indexed with integer
+function void test_assoc_array();
+  //An associative array of integers indexed with some string
+  int assoc_test[string];
+  string indx;
+  assoc_test["A"] = 1; 
+  assoc_test["B"] = 2; 
+  assoc_test["C"] = 3; 
+  if(assoc_test.first(indx)) begin
+    do begin
+      $display("assoc_test[%s]=%0d",indx,assoc_test[indx]);
+    end while (assoc_test.next(indx));
+  end  
+endfunction
+
+initial begin
+  test_assoc_array();
+end
+
+endmodule
